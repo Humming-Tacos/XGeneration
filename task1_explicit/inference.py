429c80d88c6c6bfa77bfecec4fc7c5de74332307
Xuening0322 <xuening.wang19@student.xjtlu.edu.cn>
2022-07-30 23:40:59 +0800

    Add quantization to task1

--- task1_explicit/inference.py
+++ task1_explicit/inference.py
@@ -2,7 +2,7 @@ import os.path
 import sys
 import time
 import datetime
-
+import math
 import mido
 import numpy as np
 import pretty_midi
@@ -74,6 +74,69 @@ def extract_rhythm(x, hold_token=2, rest_token=3):
         else:
             d.append(rest_token)
     return np.array(d)
+
+
+def clamp_midi_data(input_midi_data):
+    tempo = input_midi_data.get_tempo_changes()[1][0]
+
+    original_end_time = input_midi_data.instruments[0].notes[-1].end
+    # end_time -> beats
+    original_end_beat = (original_end_time / 60) * tempo
+
+    end_time = math.ceil((60 / tempo) * original_end_beat / 2) * 2
+    # end_time = input_midi_data.tick_to_time(last_tick)
+    # extends first and last note to the end
+    input_midi_data.instruments[0].notes[0].start = 0
+    input_midi_data.instruments[0].notes[-1].end = end_time
+
+
+def quantize_midi_data(input_midi_data: pretty_midi.PrettyMIDI):
+    starts = input_midi_data.get_onsets()
+    tempo = input_midi_data.get_tempo_changes()[1][0]
+
+    def quantize(time):
+
+        beat = round((time / 60) * tempo * 8) / 8
+
+        new_time = ((beat / tempo) * 60)
+
+        return new_time
+
+    # get ends:
+    ends = [note.end for note in input_midi_data.instruments[0].notes]
+    delete_notes_index = []
+
+    for i, note in enumerate(input_midi_data.instruments[0].notes):
+        note.start = quantize(note.start)
+        note.end = quantize(note.end)
+        if i != 0:
+            if note.start == input_midi_data.instruments[0].notes[i - 1].start:
+                # choose longest => choose last end
+                if note.end > input_midi_data.instruments[0].notes[i - 1].end:
+                    delete_notes_index.append(i - 1)
+                else:
+                    delete_notes_index.append(i)
+            elif note.start >= input_midi_data.instruments[0].notes[i - 1].start and note.start < \
+                    input_midi_data.instruments[0].notes[i - 1].end:
+                input_midi_data.instruments[0].notes[i - 1].end = note.start
+
+    input_midi_data.instruments[0].notes = [
+        note for i, note in enumerate(input_midi_data.instruments[0].notes) if i not in delete_notes_index
+    ]
+
+
+def _print_notes(input_midi_data):
+    for note in input_midi_data.instruments[0].notes:
+        print(note)
+
+
+def preprocess_midi(input_midi_path):
+    input_midi_data = pretty_midi.PrettyMIDI(input_midi_path)
+    clamp_midi_data(input_midi_data)
+    quantize_midi_data(input_midi_data)
+    output_midi_path = input_midi_path
+    input_midi_data.write(output_midi_path)
+    return output_midi_path
 
 
 def processed_data_tensor(data):
@@ -326,6 +389,8 @@ def change_tempo(midi):
 def inference(midi_path):
     print(torch.cuda.is_available())
     res = []
+    # quantize midi first
+    midi_path = preprocess_midi(midi_path)
     change_tempo(midi_path)
     process_note_time(midi_path)
     min_step = 1 / 12
